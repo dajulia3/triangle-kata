64f1216e1657d72ea77f7d65ff1709d764956586
David Julia <dajulia3@gmail.com>
2015-11-27 17:43:12 -0600

    Delete test for negative triangle sides- covered by impossible triangle test

--- src/test/java/triangles/TriangleClassifierTest.java
+++ src/test/java/triangles/TriangleClassifierTest.java
@@ -28,17 +28,7 @@ public class TriangleClassifierTest {
         int id = classifier.identifyTriangle(5, 4, 3);
         assertThat("scalene triangle should return id of 3", id, equalTo(3));
     }
-
-    @Test
-    public void identifyTriangle_throwsException_whenTriangleSideIsNegative(){
-        TriangleClassifier classifier = new TriangleClassifier();
-        try {
-            classifier.identifyTriangle(-1, 2, 3);
-            fail();
-        } catch (TriangleClassifier.TriangleMalformedException e) {
-        }
-    }
-
+    
     @Test
     public void identifyTriangle_throwsException_whenTriangleViolatesTriangleInequalityTheorem(){
         TriangleClassifier classifier = new TriangleClassifier();
