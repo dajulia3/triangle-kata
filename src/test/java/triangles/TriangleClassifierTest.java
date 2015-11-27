b37fb1b12ef98912a49f7b7afa9ae77b2a47dd38
David Julia <dajulia3@gmail.com>
2015-11-27 16:24:28 -0600

    rename test methods to reflect real method name

--- src/test/java/triangles/TriangleClassifierTest.java
+++ src/test/java/triangles/TriangleClassifierTest.java
@@ -9,28 +9,28 @@ import static org.junit.Assert.*;
 public class TriangleClassifierTest {
 
     @Test
-    public void triangleType_returns1_whentriangleIsEquilateral() {
+    public void identifyTriangle_returns1_whentriangleIsEquilateral() {
         TriangleClassifier classifier = new TriangleClassifier();
         int id = classifier.identifyTriangle(1, 1, 1);
         assertThat("equilateral triangle should return id of 1", id, equalTo(1));
     }
 
     @Test
-    public void triangleType_returns2_whentriangleIsIsosceles() {
+    public void identifyTriangle_returns2_whentriangleIsIsosceles() {
         TriangleClassifier classifier = new TriangleClassifier();
         int id = classifier.identifyTriangle(1, 2, 2);
         assertThat("isosceles triangle should return id of 2", id, equalTo(2));
     }
 
     @Test
-    public void triangleType_returns3_whentriangleIsScalene() {
+    public void identifyTriangle_returns3_whentriangleIsScalene() {
         TriangleClassifier classifier = new TriangleClassifier();
         int id = classifier.identifyTriangle(5, 4, 3);
         assertThat("scalene triangle should return id of 3", id, equalTo(3));
     }
 
     @Test
-    public void triangleType_throwsException_whenTriangleSideIsNegative(){
+    public void identifyTriangle_throwsException_whenTriangleSideIsNegative(){
         TriangleClassifier classifier = new TriangleClassifier();
         try {
             classifier.identifyTriangle(-1, 2, 3);
@@ -40,7 +40,7 @@ public class TriangleClassifierTest {
     }
 
     @Test
-    public void triangleType_throwsException_whenTriangleViolatesTriangleInequalityTheorem(){
+    public void identifyTriangle_throwsException_whenTriangleViolatesTriangleInequalityTheorem(){
         TriangleClassifier classifier = new TriangleClassifier();
         try {
             classifier.identifyTriangle(3, 2, 1);
